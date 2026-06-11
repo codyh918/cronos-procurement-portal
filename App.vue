3b6d78f2904185c5dff129b32938c98ca7441abe
codyh918 <codyhibbard22@gmail.com>
2026-06-11 06:22:58 -0400

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <RouterView v-if="isPublicOrderTracking" />
+  <AuthGate v-else>
+    <AppShell>
+      <RouterView />
+    </AppShell>
+  </AuthGate>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import AppShell from './components/AppShell.vue'
+import AuthGate from './components/AuthGate.vue'
+
+const route = useRoute()
+const isPublicOrderTracking = computed(() => route.path.startsWith('/orders/track'))
+</script>
