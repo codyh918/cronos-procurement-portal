6c9d91753cb7370d283737072e5c645d6b48fbde
codyh918 <codyhibbard22@gmail.com>
2026-06-11 07:45:11 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: { port: 5002 },
+  preview: {
+    allowedHosts: ['cronos-procurement-portal-production.up.railway.app'],
+  },
 })
