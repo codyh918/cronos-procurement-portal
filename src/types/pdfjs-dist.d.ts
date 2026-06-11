ffb9ba12df7fb78333d943364723a33731ea91f7
codyh918 <codyhibbard22@gmail.com>
2026-06-11 13:57:06 -0400

    Delete src directory

--- src/types/pdfjs-dist.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'pdfjs-dist/build/pdf.mjs' {
-  export const GlobalWorkerOptions: { workerSrc: string }
-  export function getDocument(input: unknown): { promise: Promise<unknown> }
-}
